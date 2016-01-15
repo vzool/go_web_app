4e727fdb5b18552b1e7ad01d8d2c3b29d8051e58
Abdelaziz Elrashed <aeemh.sdn@gmail.com>
2016-01-15 19:06:53 +0300

    fix Content-Type

--- src/main/main.go
+++ src/main/main.go
@@ -63,7 +63,7 @@ func serveResource(w http.ResponseWriter, req *http.Request) {
 
     if err == nil {
         defer f.Close()
-        w.Header().Add("Content Type", contentType)
+        w.Header().Add("Content-Type", contentType)
         br := bufio.NewReader(f)
         br.WriteTo(w)
     } else {
